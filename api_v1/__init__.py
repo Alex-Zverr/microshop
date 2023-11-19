adcc74846d7582ed50fe67e8d20f1fae30954a67
Alex <Can9l@live.ru>
2023-11-20 00:35:19 +0300

    Products Api

---
+++ api_v1/__init__.py
@@ -0,0 +1,5 @@
+from fastapi import APIRouter
+from .products.view import router as products_router
+
+router = APIRouter()
+router.include_router(router=products_router, prefix="/products")
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from core.config import settings
+from core.models import Base, db_helper
+from api_v1 import router as router_v1
+from items_views import router as items_router
+from users.views import router as users_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+app.include_router(items_router)
+app.include_router(users_router)
 
 
 @app.get("/")
@@ -8,6 +28,16 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
+@app.get("/hello")
+def hello(name: str = "World"):
+    name = name.strip().title()
     return {"message": f"Hello {name}"}
+
+
+@app.post("/calc/add/")
+def add(a: int, b: int):
+    return {
+        'a': a,
+        'b': b,
+        'result': a + b
+    }
