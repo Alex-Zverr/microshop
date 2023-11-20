176f7e752c4b7cb75aaa62c19e9b700969b13f98
Programmer <Cah9l@live.ru>
2023-11-20 16:58:40 +0300

    fix bub scoped session dependency

--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -21,14 +21,19 @@ class DatabaseHelper:
     def get_scoped_session(self):
         session = async_scoped_session(
             session_factory=self.session_factory,
-            scopefunc=current_task
+            scopefunc=current_task,
         )
         return session
 
     async def session_dependency(self) -> AsyncSession:
-        async with self.get_scoped_session() as session:
+        async with self.session_factory() as session:
             yield session
-            await session.remove()
+            await session.close()
+
+    async def scoped_session_dependency(self) -> AsyncSession:
+        session = self.get_scoped_session()
+        yield session
+        await session.remove()
 
 
 db_helper = DatabaseHelper(
