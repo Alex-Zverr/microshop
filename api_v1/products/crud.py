1ada8c5b26d0783fe1d6a9db2649ad941691b613
Programmer <Cah9l@live.ru>
2023-11-21 18:03:35 +0300

    work commit

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -3,14 +3,14 @@ from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import Product
 
-from .shemas import ProductCreate
+from .shemas import ProductCreate, ProductUpdate, ProductUpdatePartial
 
 
 async def get_products(session: AsyncSession) -> list[Product]:
     stmt = select(Product).order_by(Product.id)
     result: Result = await session.execute(stmt)
     products = result.scalars().all()
-    return products
+    return list(products)
 
 
 async def get_product(session: AsyncSession, product_id: int) -> Product | None:
@@ -24,3 +24,22 @@ async def create_product(session: AsyncSession, product_in: ProductCreate) -> Pr
     # await session.refresh(product)
     return product
 
+
+async def update_product(
+        session: AsyncSession,
+        product: Product,
+        product_update: ProductUpdate | ProductUpdatePartial,
+        partial: bool = False
+) -> Product:
+    for name, value in product_update.model_dump(exclude_unset=partial).items():
+        setattr(product, name, value)
+    await session.commit()
+    return product
+
+
+async def delete_product(
+        session: AsyncSession,
+        product: Product
+) -> None:
+    await session.delete(product)
+    await session.commit()
