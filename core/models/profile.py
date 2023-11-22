58e95572726b9c7fdde8cd4b41c2e32d694d47be
Alex <Can9l@live.ru>
2023-11-22 23:14:34 +0300

    Create table PROFILE

--- core/models/profile.py
+++ core/models/profile.py
@@ -1,16 +1,17 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
-
-if TYPE_CHECKING:
-    from .user import User
+from .mixins import UserRelationMixin
 
 
-class Profile(Base):
+class Profile(UserRelationMixin, Base):
+    _user_id_unique = True
+    _user_back_populate = "profile"
+
     first_name: Mapped[str | None] = mapped_column(String(40))
     last_name: Mapped[str | None] = mapped_column(String(40))
     bio: Mapped[str | None]
-    user_id: Mapped[int] = mapped_column(ForeignKey("users_id"), unique=True)
+
